4b1abbed2e9dff04cac23b0d1f78593142a1869b
JordiMasci <jordimasci96@gmail.com>
2025-02-11 17:01:40 +0100

    start navbarRouting

--- src/components/NavbarRouting.jsx
+++ src/components/NavbarRouting.jsx
@@ -1,1 +1,17 @@
-function NavbarRouting() {}
+import { Link } from "react-router-dom";
+
+function NavbarRouting() {
+  return (
+    <ul>
+      <li>
+        <Link to={""}>Pagina 1</Link>
+        <Link to={""}>Pagina 2</Link>
+        <Link to={""}>Pagina 3</Link>
+        <Link to={""}>Pagina 4</Link>
+        <Link to={""}>Pagina 5</Link>
+      </li>
+    </ul>
+  );
+}
+
+export default NavbarRouting;
