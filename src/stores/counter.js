db35f4d863843f2bc2152801da8bbe26bca0e984
JordiMasci <jordimasci96@gmail.com>
2025-02-12 09:33:10 +0100

    .

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const counterSlice = createSlice({
   name: "counter",
-  initialState: { count: 10 },
+  initialState: { count: 0 },
   reducers: {
     increment: (state) => {
       state.count += 1;
